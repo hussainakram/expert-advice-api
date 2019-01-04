9d6a4cd4b63c712b1d21ab59c71a365e866c8c50
hussainakram <hussain.akram128@gmail.com>
2019-01-05 02:43:48 +0500

    Answer: add nested routes for answers

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/users/me', to: 'users#me'
       resources :users
-      resources :questions
+      resources :questions do
+        resources :answers
+      end
     end
   end
 end
